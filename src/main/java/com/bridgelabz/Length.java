5b7cc46ae4e6d5b08e8c079e875edbbe80c0ea4f
RajendraGund <rajendragund2015@gmail.com>
2021-11-09 08:09:57 +0530

    Added : Refactored code using enum Feet and Inch

--- src/main/java/com/bridgelabz/Length.java
+++ src/main/java/com/bridgelabz/Length.java
@@ -5,18 +5,22 @@ package com.bridgelabz;
  * @version -> 1.0
  * @since -> 09/11/2021
  */
-public class Feet {
+public class Length {
+    enum Unit {FEET, INCH}
+
+    private final Unit unit;
     private final double value;
 
-    public Feet(double value) {
+    public Length(Unit unit, double value) {
         this.value = value;
+        this.unit = unit;
     }
 
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
-        if (!(o instanceof Feet)) return false;
-        Feet feet = (Feet) o;
+        if (!(o instanceof Length)) return false;
+        Length feet = (Length) o;
         return Double.compare(feet.value, value) == 0;
     }
 }
--- src/test/java/com/bridgelabz/QuantityTest.java
+++ src/test/java/com/bridgelabz/QuantityTest.java
@@ -7,71 +7,71 @@ public class QuantityTest {
 
     @Test
     public void given0FeetAnd0Feet_ShouldReturnEqual() {
-        Feet feet1 = new Feet(0.0);
-        Feet feet2 = new Feet(0.0);
+        Length feet1 = new Length(Length.Unit.FEET, 0.0);
+        Length feet2 = new Length(Length.Unit.FEET, 0.0);
         Assert.assertEquals(feet1, feet2);
     }
 
     @Test
     public void given0FeetAnd1Feet_ShouldReturnNotEqual() {
-        Feet feet1 = new Feet(0.0);
-        Feet feet2 = new Feet(1.0);
+        Length feet1 = new Length(Length.Unit.FEET, 0.0);
+        Length feet2 = new Length(Length.Unit.FEET, 1.0);
         Assert.assertNotEquals(feet1, feet2);
     }
 
     @Test
     public void given0FeetAndNull_ShouldReturnNotEqual() {
-        Feet feet1 = new Feet(0.0);
-        Feet feet2 = null;
+        Length feet1 = new Length(Length.Unit.FEET, 0.0);
+        Length feet2 = null;
         Assert.assertNotEquals(feet1, feet2);
     }
 
     @Test
     public void givenFeetRefAreSame_ShouldReturnTrue() {
-        Feet feet1 = new Feet(0.0);
+        Length feet1 = new Length(Length.Unit.FEET, 0.0);
         boolean result = feet1 == feet1;
         Assert.assertTrue(result);
     }
 
     @Test
     public void given0FeetAnd1FeetFromDiffType_ShouldReturnNotSame() {
-        Feet feet1 = new Feet(0.0);
-        Feet feet2 = new Feet(1.0);
+        Length feet1 = new Length(Length.Unit.FEET, 0.0);
+        Length feet2 = new Length(Length.Unit.FEET, 1.0);
         Assert.assertNotSame(feet1, feet2);
     }
 
     @Test
     public void given0InchAnd0Inch_ShouldReturnEqual() {
-        Inch inch1 = new Inch(0.0);
-        Inch inch2 = new Inch(0.0);
+        Length inch1 = new Length(Length.Unit.INCH,0.0);
+        Length inch2 = new Length(Length.Unit.INCH,0.0);
         Assert.assertEquals(inch1, inch2);
     }
 
     @Test
     public void given0Inchand1Inch_ShouldReturnNotEqual() {
-        Inch inch1 = new Inch(0.0);
-        Inch inch2 = new Inch(1.0);
+        Length inch1 = new Length(Length.Unit.INCH,0.0);
+        Length inch2 = new Length(Length.Unit.INCH,1.0);
         Assert.assertNotEquals(inch1, inch2);
     }
 
     @Test
     public void given0InchandNullInch_ShouldReturnNotEqual() {
-        Inch inch1 = new Inch(0.0);
-        Inch inch2 = null;
+        Length inch1 = new Length(Length.Unit.INCH,0.0);
+        Length inch2 = null;
         Assert.assertNotEquals(inch1, inch2);
     }
 
     @Test
     public void givenInchandInchFromSameRefAreEqual_ShouldReturnTrue() {
-        Inch inch1 = new Inch(0);
+        Length inch1 = new Length(Length.Unit.INCH,0);
         boolean result = inch1 == inch1;
         Assert.assertTrue(result);
     }
 
     @Test
     public void given0Inchand0InchFromDifferentType_ShouldReturnNotSame() {
-        Inch inch1 = new Inch(0.0);
-        Inch inch2 = new Inch(0);
+        Length inch1 = new Length(Length.Unit.INCH,0.0);
+        Length inch2 = new Length(Length.Unit.INCH,0);
         Assert.assertNotSame(inch1, inch2);
     }
 }
