06313d948d17f0c4cdb27e5f9adf5849de69fbdb
RajendraGund <rajendragund2015@gmail.com>
2021-11-09 06:34:53 +0530

    Added : Test case for check given feet type are same or not

--- src/test/java/com/bridgelabz/QuantityTest.java
+++ src/test/java/com/bridgelabz/QuantityTest.java
@@ -25,10 +25,18 @@ public class QuantityTest {
         Feet feet2 = null;
         Assert.assertNotEquals(feet1, feet2);
     }
+
     @Test
     public void givenFeetRefAreSame_ShouldReturnTrue() {
         Feet feet1 = new Feet(0.0);
-       boolean result = feet1==feet1;
+        boolean result = feet1 == feet1;
         Assert.assertTrue(result);
+    }
+
+    @Test
+    public void given0FeetAnd1FeetFromDiffType_ShouldReturnNotSame() {
+        Feet feet1 = new Feet(0.0);
+        Feet feet2 = new Feet(1.0);
+        Assert.assertNotSame(feet1, feet2);
     }
 }
