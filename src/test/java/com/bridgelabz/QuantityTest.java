f8da0216f8e24f443801d664f2fa2892bc3c5009
RajendraGund <rajendragund2015@gmail.com>
2021-11-09 06:44:29 +0530

    Added : Test case for check equality given 0 inch and 1 inch

--- src/test/java/com/bridgelabz/QuantityTest.java
+++ src/test/java/com/bridgelabz/QuantityTest.java
@@ -46,4 +46,10 @@ public class QuantityTest {
         Inch inch2 = new Inch(0.0);
         Assert.assertEquals(inch1, inch2);
     }
+    @Test
+    public void given0Inchand1Inch_ShouldReturnNotEqual() {
+        Inch inch1 = new Inch(0.0);
+        Inch inch2 = new Inch(1.0);
+        Assert.assertNotEquals(inch1, inch2);
+    }
 }
