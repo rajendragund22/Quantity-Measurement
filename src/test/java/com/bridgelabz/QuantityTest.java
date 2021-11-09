d6ac6452221f7f0cc4aa0d7fc1094ed6570d4fec
RajendraGund <rajendragund2015@gmail.com>
2021-11-09 06:29:56 +0530

    Added : Test case for check feet ref is true or false

--- src/test/java/com/bridgelabz/QuantityTest.java
+++ src/test/java/com/bridgelabz/QuantityTest.java
@@ -6,23 +6,29 @@ import org.junit.Test;
 public class QuantityTest {
 
     @Test
-    public void whenGiven0FeetAnd0Feet_ShouldReturnEqual() {
+    public void given0FeetAnd0Feet_ShouldReturnEqual() {
         Feet feet1 = new Feet(0.0);
         Feet feet2 = new Feet(0.0);
         Assert.assertEquals(feet1, feet2);
     }
 
     @Test
-    public void whenGiven0FeetAnd1Feet_ShouldReturnNotEqual() {
+    public void given0FeetAnd1Feet_ShouldReturnNotEqual() {
         Feet feet1 = new Feet(0.0);
         Feet feet2 = new Feet(1.0);
         Assert.assertNotEquals(feet1, feet2);
     }
 
     @Test
-    public void whenGiven0FeetAndNull_ShouldReturnNotEqual() {
+    public void given0FeetAndNull_ShouldReturnNotEqual() {
         Feet feet1 = new Feet(0.0);
         Feet feet2 = null;
         Assert.assertNotEquals(feet1, feet2);
+    }
+    @Test
+    public void givenFeetRefAreSame_ShouldReturnTrue() {
+        Feet feet1 = new Feet(0.0);
+       boolean result = feet1==feet1;
+        Assert.assertTrue(result);
     }
 }
