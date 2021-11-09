4ab54452ecd662f45ebbf1829031a90982af1343
RajendraGund <rajendragund2015@gmail.com>
2021-11-09 06:01:16 +0530

    Added : Test case to check equality of  0 feet and 0 feet

---
+++ src/main/java/com/bridgelabz/Feet.java
@@ -0,0 +1,17 @@
+package com.bridgelabz;
+
+public class Feet {
+    public final double value;
+
+    public Feet(double value) {
+        this.value = value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Feet)) return false;
+        Feet feet = (Feet) o;
+        return Double.compare(feet.value, value) == 0;
+    }
+}
